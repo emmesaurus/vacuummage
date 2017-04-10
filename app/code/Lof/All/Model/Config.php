294a0011be732cf308a39bd50346a6a5dacd1071
Eden Thompson <emmesaurus@gmail.com>
2017-04-10 11:20:11 -0700

    added hide price plugin

---
+++ app/code/Lof/All/Model/Config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Landofcoder
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the venustheme.com license that is
+ * available through the world-wide-web at this URL:
+ * http://venustheme.com/license
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ * 
+ * @category   Landofcoder
+ * @package    Lof_All
+ * @copyright  Copyright (c) 2017 Landofcoder (http://www.venustheme.com/)
+ * @license    http://www.venustheme.com/LICENSE-1.0.html
+ */
+
+namespace Lof\All\Model;
+
+class Config
+{
+	const LANDOFCODER_TICKET = 'https://landofcoder.ticksy.com/';
+	const MEGAMENU_GUIDE     = 'http://guide.landofcoder.com/mega-menu/';
+	const BLOG_GUIDE         = 'http://guide.landofcoder.com/m2-blog/';
+	const FORMBUILDER_GUIDE  = 'http://guide.landofcoder.com/formbuilder/';
+	const HIDEPRICE_GUIDE    = 'http://guide.landofcoder.com/hideprice/';
+	const FAQ_GUIDE    		 = 'http://guide.landofcoder.com/faq/';
+	const GALLERY_GUIDE  	 = 'http://guide.landofcoder.com/image-gallery-magento-2/';
+}
